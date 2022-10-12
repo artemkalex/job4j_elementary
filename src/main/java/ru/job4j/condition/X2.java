e9dfe2bbbc5f3ca16b71bc57383635677ce04b06
artemlex <artemlex@gmail.com>
2022-10-12 08:47:43 +0300

    1.2. Входные данные в тесте [#297750 #350601]

--- src/main/java/ru/job4j/condition/X2.java
+++ src/main/java/ru/job4j/condition/X2.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class X2 {
 
     public static int calc(int a, int b, int c, int x) {
-        return a * x * x + b * x * c;
+        return a * x * x + b * x + c;
     }
 
     public static void main(String[] args) {
